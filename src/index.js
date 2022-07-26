ecc08715234c8cb7a5bc58d6d31965010d2e37fd
jaimit25 <jaimit.jp@gmail.com>
2022-07-26 12:22:35 +0530

    abhinav

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-import App from './App';
+import Layout from './layout'
+import Home from './components/home'
+const App = () => {
+    return (
+        <div>
+            <Layout>        
+                <Router>
+                    <Routes>
+                        <Route path = "/" element={<Home/>}/>
+                    </Routes>
+                </Router>
+            </Layout>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
     <App />
-
 );
 
