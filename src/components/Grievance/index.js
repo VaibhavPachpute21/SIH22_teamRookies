05c53bf427c218d8124d4274b8d5c9ef2b5b7de6
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-08-03 22:42:48 +0530

    grievance

--- src/components/Grievance/index.js
+++ src/components/Grievance/index.js
@@ -1,26 +1,27 @@
-import { Avatar, Box, Flex, HStack, VStack, Image, Heading, Text, Spacer, IconButton } from '@chakra-ui/react'
-import {TiExportOutline} from 'react-icons/ti'
+import { Avatar, Box, Flex, HStack, VStack, Image, Heading, Text, Spacer, IconButton, Divider } from '@chakra-ui/react'
+import { TiExportOutline } from 'react-icons/ti'
 
 const Grievance = () => {
 
     return (
         <Box w="100vw"
-            h="100vh">
-                <Box h="5%" w="95%">
-                    <Flex w="100%" h="100%"> 
-                        <Box
-                         w="100%" h="100%">
-                            <Flex w="100%" h="100%" justifyContent={'flex-end'} alignItems={'center'}>
+            h="100%" bg={'pink.100'} >
+            <Box h="5%" w="95%">
+                <Flex w="100%" h="100%">
+                    <Box
+                        w="100%" h="100%">
+                        <Flex w="100%" h="100%" justifyContent={'flex-end'} alignItems={'center'}>
                             <IconButton
-                            color={'white'}
-                            background={'#'}
-                            icon={<TiExportOutline/>}/>
-                            </Flex>
-                        </Box>
-                    </Flex>
-                </Box>
-            <Flex w="100%" h="95%" flexDir={'column'} alignItems={'center'} justifyContent={'center'}>
-                <Box w="80%" h="100%"  >
+                                fontSize={24}
+                                color={'red'}
+                                background={'#'}
+                                icon={<TiExportOutline />} />
+                        </Flex>
+                    </Box>
+                </Flex>
+            </Box>
+            <Flex w="100%" h="95%" flexDir={'column'} alignItems={'center'} justifyContent={'center'} paddingBottom={5} >
+                <Box w="80%" h="100%"   bg={'whitesmoke'} borderRadius={15}>
                     <VStack w="100%" h="100%">
                         <HStack
                             spacing={5}
@@ -39,7 +40,7 @@ const Grievance = () => {
                                         justifyContent={'flex-start'} w="100%" h="20%">
                                         <Heading
                                             pt={4}
-                                            fontSize={['xl','2x;','5xl']}
+                                            fontSize={['xl', '2x;', '5xl']}
                                             fontFamily={'monospace'}
                                             fontWeight={600}>
                                             Grievance form
@@ -54,7 +55,7 @@ const Grievance = () => {
 
                                     <HStack w="100%" h="20%">
                                         <Text
-                                            fontSize={['sm','md','lg']}
+                                            fontSize={['sm', 'md', 'lg']}
                                             fontWeight={700}
                                             w="100%" textAlign={'flex-start'} h="50%">
                                             University Grants Commission (UGC) Bahadur Shah Zafar Marg, New Delhi - 110002.
@@ -64,16 +65,16 @@ const Grievance = () => {
                                     <HStack w="100%" h="10%">
                                         <Box py={5} h="100%">
                                             <Text
-                                            fontSize={['sm','md','lg']}
-                                            h="100%">
+                                                fontSize={['sm', 'md', 'lg']}
+                                                h="100%">
                                                 <strong>Phone number</strong>: 2498-108-01824
                                             </Text>
                                         </Box>
 
                                         <Box py={5} h="100%">
-                                            <Text 
-                                            fontSize={['sm','md','lg']}
-                                            h="100%">
+                                            <Text
+                                                fontSize={['sm', 'md', 'lg']}
+                                                h="100%">
                                                 <strong>Email address</strong>: vaibhav@gmail.com
                                             </Text>
                                         </Box>
@@ -81,46 +82,46 @@ const Grievance = () => {
                                 </VStack>
                             </Box>
                         </HStack>
-                        <VStack w="100%" h="90%" spacing={10}>
-                        <Box borderRadius={8} padding={2} width={'90%'}>
-                            <Heading textAlign={'center'} >Grievant(s) Information</Heading>
-                            <Box w={'100%'}>
-                                
-                                <Box  padding={2} border={'1px solid black'} w={'100%'}><Text ><strong>Grievant's Name:</strong>Vaibhav</Text></Box>
-                                <Flex padding={2} flexDirection={'row'} border={'1px solid black'} w={'100%'}>
-                                    <Box  w="50%"><Text><strong>Email:</strong>Vaibhav</Text></Box>
-                                    <Box w="50%"><Text><strong>Grievance Type:</strong>clssification</Text></Box>
-                                </Flex>
-                                <Flex padding={2} flexDirection={'row'} border={'1px solid black'} w={'100%'}>
-                                    <Box w="50%"><Text><strong>Grievant To:</strong>Vaibhav</Text></Box>
-                                    <Box w="50%"><Text><strong>Grievance Filed on:</strong>22-09-22</Text></Box>
-                                </Flex>
-                            </Box>
-                        </Box>
+                        <Box borderTop={'1px solid black'} w={'95%'}></Box>
 
-                        <Box borderRadius={8} padding={2} width={'90%'}>
-                            <Heading textAlign={'center'} >Grievance</Heading>
-                            <Box w={'100%'}>
-                                
-                                <Box  padding={2} border={'1px solid black'} w={'100%'}><Text ><strong>Title of Grievance:</strong>something</Text></Box>
-                                <Flex padding={2} flexDirection={'row'} border={'1px solid black'} w={'100%'}>
-                                    <Box><Text><strong>Sender:</strong>Grievant name</Text></Box>
-                                </Flex>
-                                <Flex padding={2} flexDirection={'row'} border={'1px solid black'} w={'100%'}>
-                                    <Box><Text><strong>Grievance Raised for:</strong>ugc dept name</Text></Box>
-                                </Flex>
-                                <Flex padding={2} flexDirection={'row'} border={'1px solid black'} w={'100%'}>
-                                    <Box w="50%"><Text><strong>Current Status:</strong>Pending</Text></Box>
-                                </Flex>
+                        <VStack w="100%" h="100%" spacing={5} paddingBottom={10} >
+                            <Box borderRadius={8} padding={2} width={'90%'}>
+                                <Heading paddingBottom={5} textAlign={'center'} >Grievant(s) Information</Heading>
+                                <Box w={'100%'}>
+                                    <Box padding={2} border={'1px solid black'} w={'100%'}><Text fontSize={22}><strong>Grievant's Name:</strong>Vaibhav</Text></Box>
+                                    <Flex padding={2} flexDirection={'row'} border={'1px solid black'} w={'100%'}>
+                                        <Box w="50%"><Text fontSize={22}><strong>Email:</strong>Vaibhav</Text></Box>
+                                        <Box w="50%"><Text fontSize={22}><strong>Grievance Type:</strong>clssification</Text></Box>
+                                    </Flex>
+                                    <Flex padding={2} flexDirection={'row'} border={'1px solid black'} w={'100%'}>
+                                        <Box w="50%"><Text fontSize={22}><strong>Grievant To:</strong>Vaibhav</Text></Box>
+                                        <Box w="50%"><Text fontSize={22}><strong>Grievance Filed on:</strong>22-09-22</Text></Box>
+                                    </Flex>
+                                </Box>
                             </Box>
-                        </Box>
+                            <Box padding={2} width={'90%'}>
+                                <Heading textAlign={'center'} paddingBottom={5} >Grievance</Heading>
+                                <Box w={'100%'}>
+
+                                    <Box padding={2} border={'1px solid black'} w={'100%'}><Text fontSize={22}><strong>Title of Grievance:</strong>Improper Canteen Fascility</Text></Box>
+                                    <Flex padding={2} flexDirection={'row'} border={'1px solid black'} w={'100%'}>
+                                        <Box><Text fontSize={22}><strong>Sender:</strong>Grievant name</Text></Box>
+                                    </Flex>
+                                    <Flex padding={2} flexDirection={'row'} border={'1px solid black'} w={'100%'}>
+                                        <Box><Text fontSize={22}><strong>Grievance Raised for:</strong>ugc dept name</Text></Box>
+                                    </Flex>
+                                    <Flex padding={2} flexDirection={'row'} border={'1px solid black'} w={'100%'}>
+                                        <Box w="50%"><Text fontSize={22}><strong>Current Status:</strong>Pending</Text></Box>
+                                    </Flex>
+                                </Box>
+                            </Box>
 
 
-                        <Box borderRadius={8} padding={2} width={'90%'} >
-                            <Box w={'100%'}>
-                                <Box padding={2} h={'15vh'} border={'1px solid black'} w={'100%'}><Text ><strong>Description of Grievance:</strong>something</Text></Box>
+                            <Box borderRadius={8} padding={2} width={'90%'} >
+                                <Box w={'100%'}>
+                                    <Box padding={2} h={'100%'} border={'1px solid black'} w={'100%'}><Text fontSize={22}><strong>Description of Grievance:</strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</Text></Box>
+                                </Box>
                             </Box>
-                        </Box>
                         </VStack>
                     </VStack>
 
