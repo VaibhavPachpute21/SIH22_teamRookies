6bfcff411fc65b73744b66276d3cc5978ac33552
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-08-04 17:21:37 +0530

    yup removed

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -4,8 +4,6 @@ import { UserAuth } from '../../context/AuthContext'
 import { Box, Flex, Input, Icon, FormControl, SimpleGrid, GridItem, Heading, FormLabel, Textarea, Checkbox, Button, Image, Select } from '@chakra-ui/react';
 import { Link, useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
 
 const initState = {
   fName: "",
@@ -24,25 +22,9 @@ const initState = {
 
 }
 
-let schema = yup.object().shape({
-  fName: yup.string().required(),
-  lName: yup.string().required(),
-  phone: yup.number().required().min(10),
-  collage:yup.string().required(),
-  enrollment:yup.number().required(),
-  course:yup.string().required(),
-  email:yup.string().required(),
-  role:yup.string().required(),
-  pass:yup.string().required().min(6),
-  DOB:yup.date().required(),
-  academicYear:yup.string().required(),
-  address:yup.string().required(),
-});
 
 export default function Registration() {
-  const { register, handleSubmit, formState: { errors } } = useForm({
-    resolver: yupResolver(schema),
-  });
+  const { register, handleSubmit, formState: { errors } } = useForm({ mode: 'onChange' });
 
   const [inputState, setInputState] = useState(initState)
   const { fName, lName, phone, collage, enrollment, course, email, role, pass, cpassword, DOB, academicYear, address } = inputState;
@@ -86,60 +68,115 @@ export default function Registration() {
               >
                 <GridItem>
                   <FormLabel>First Name</FormLabel>
-                  <Input type={'text'} name={"fName"} {...register('fName')} id="fName" value={fName} onChange={handleInputChnage} placeholder='Name' />
-                  <span>{errors.fName && <p style={{ color: 'red' }}>Please Enter First Name!</p>}</span>
+                  <Input type={'text'} name={"fName"} {...register('fName',{required:{value: true,message: "Name is required!",} })} id="fName" value={fName} onChange={handleInputChnage} placeholder='Name' />
+                  {errors.fName && errors.fName.message ? (
+                          <Box textAlign={'left'} fontSize={'12px'} py={1} maxH={'0px'} color={'red'}>
+                            {errors.fName.message}
+                          </Box>
+                        ) : (null)
+                      }
                 </GridItem>
 
                 <GridItem>
                   <FormLabel>Last Name</FormLabel>
-                  <Input type={'text'} name={"lName"}  {...register('lName')} id="lName" value={lName} onChange={handleInputChnage} placeholder='Name' />
-                  <span>{errors.lName && <p style={{ color: 'red' }}>Please Enter Last Name!</p>}</span>
+                  <Input type={'text'} name={"lName"}  {...register('lName',{required:{value: true,message: "Last Name is required!",}})} id="lName" value={lName} onChange={handleInputChnage} placeholder='Name' />
+                  {errors.lName && errors.lName.message ? (
+                          <Box textAlign={'left'} fontSize={'12px'} py={1} maxH={'0px'} color={'red'}>
+                            {errors.lName.message}
+                          </Box>
+                        ) : (null)
+                      }
                 </GridItem>
 
                 <GridItem>
                   <FormLabel>Phone No.</FormLabel>
-                  <Input type={'tel'} name="phone" {...register('phone')} id="phone" value={phone} onChange={handleInputChnage} placeholder='Phone No.' />
-                  <span>{errors.phone && <p style={{ color: 'red' }}>Please Enter Valid Phone!</p>}</span>
+                  <Input type={'tel'} name="phone" {...register('phone',{required:{value: true,message: "Phone number is required!",}, minLength:{value:10,message: "Please Enter Valid number"} })} id="phone" value={phone} onChange={handleInputChnage} placeholder='Phone No.' />
+                  {errors.phone && errors.phone.message ? (
+                          <Box textAlign={'left'} fontSize={'12px'} py={1} maxH={'0px'} color={'red'}>
+                            {errors.phone.message}
+                          </Box>
+                        ) : (null)
+                      }
                 </GridItem>
 
                 <GridItem>
                   <FormLabel>Collage Name</FormLabel>
-                  <Input type={'text'} name="collage" {...register('collage')} value={collage} id="collage" onChange={handleInputChnage} placeholder='Collage Name' />
-                  <span>{errors.collage && <p style={{ color: 'red' }}>Collage name is required!</p>}</span>
+                  <Input type={'text'} name="collage"  value={collage} id="collage" onChange={handleInputChnage} placeholder='Collage Name' />
+                  {errors.collage && errors.collage.message ? (
+                          <Box textAlign={'left'} fontSize={'12px'} py={1} maxH={'0px'} color={'red'}>
+                            {errors.collage.message}
+                          </Box>
+                        ) : (null)
+                      }
                 </GridItem>
 
                 <GridItem>
                   <FormLabel>Enrollment Number</FormLabel>
-                  <Input type={'number'} name="enrollment" {...register('enrollment')} value={enrollment} id="enrollment" onChange={handleInputChnage} placeholder='Enrollment Number' />
-                  <span>{errors.enrollment && <p style={{ color: 'red' }}>Enrollment number is required!</p>}</span>
+                  <Input type={'number'} name="enrollment" {...register('enrollment',{required:{value: true,message: "Enrollment no. is required!",}})} value={enrollment} id="enrollment" onChange={handleInputChnage} placeholder='Enrollment Number' />
+                  {errors.enrollment && errors.enrollment.message ? (
+                          <Box textAlign={'left'} fontSize={'12px'} py={1} maxH={'0px'} color={'red'}>
+                            {errors.enrollment.message}
+                          </Box>
+                        ) : (null)
+                      }
                 </GridItem>
 
                 <GridItem>
                   <FormLabel>Course Name</FormLabel>
-                  <Input type={'text'} name="course" {...register('course')} value={course} id="course" onChange={handleInputChnage} placeholder='Course Name' />
-                  <span>{errors.course && <p style={{ color: 'red' }}>Please enter course name!</p>}</span>
+                  <Input type={'text'} name="course" {...register('course',{required:{value: true,message: "Course Name is required!",}})} value={course} id="course" onChange={handleInputChnage} placeholder='Course Name' />
+                  {errors.course && errors.course.message ? (
+                          <Box textAlign={'left'} fontSize={'12px'} py={1} maxH={'0px'} color={'red'}>
+                            {errors.course.message}
+                          </Box>
+                        ) : (null)
+                      }
                 </GridItem>
 
                 <GridItem>
                   <FormLabel>Email</FormLabel>
                   <Input type={'email'} name="email" {...register("email",
-                            {required: true,pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/})} value={email} id="email" onChange={handleInputChnage} placeholder='Email' />
+                            {
+                              required: {
+                                value: true,
+                                message: "Email is required!",
+                              },
+                              pattern: {
+                                value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                                message: "Enter valid email"
+                              }
+                            })} value={email} id="email" onChange={handleInputChnage} placeholder='Email' />
+                {errors.email && errors.email.message ? (
+                          <Box textAlign={'left'} fontSize={'12px'} py={1} maxH={'0px'} color={'red'}>
+                            {errors.email.message}
+                          </Box>
+                        ) : (null)
+                      }
                 </GridItem>
 
                 <GridItem>
                   <FormLabel>Role</FormLabel>
-                  <Select type={'text'} name="role" {...register('role')} value={role} id="role" onChange={handleInputChnage} placeholder='Select Role'>
+                  <Select type={'text'} name="role" {...register('role',{required:{value: true,message: "User role is required!",}})} value={role} id="role" onChange={handleInputChnage} placeholder='Select Role'>
                     <option>Student</option>
                     <option>Teacher</option>
                     <option>Other</option>
                   </Select>
-                  <span>{errors.role && <p style={{ color: 'red' }}>Please select role!</p>}</span>
+                  {errors.role && errors.role.message ? (
+                          <Box textAlign={'left'} fontSize={'12px'} py={1} maxH={'0px'} color={'red'}>
+                            {errors.role.message}
+                          </Box>
+                        ) : (null)
+                      }
                 </GridItem>
 
                 <GridItem>
                   <FormLabel>Password</FormLabel>
-                  <Input type={'password'} name="pass" {...register('pass')} value={pass} id="pass" onChange={handleInputChnage} placeholder='Password' />
-                  <span>{errors.pass && <p style={{ color: 'red' }}>Enter password of (min 6 characters)!</p>}</span>
+                  <Input type={'password'} name="pass" {...register('pass',{required:{value: true,message: "Choose is password",}})} value={pass} id="pass" onChange={handleInputChnage} placeholder='Password' />
+                  {errors.pass && errors.pass.message ? (
+                          <Box textAlign={'left'} fontSize={'12px'} py={1} maxH={'0px'} color={'red'}>
+                            {errors.pass.message}
+                          </Box>
+                        ) : (null)
+                      }
                 </GridItem>
 
                 <GridItem>
@@ -149,20 +186,35 @@ export default function Registration() {
 
                 <GridItem>
                   <FormLabel>Date of Birth</FormLabel>
-                  <Input type={'date'} name="DOB" {...register('DOB')} value={DOB} id="DOB" onChange={handleInputChnage} placeholder='Date of Birth' />
-                  <span>{errors.DOB && <p style={{ color: 'red' }}>Please enter date of birth</p>}</span>
+                  <Input type={'date'} name="DOB" {...register('DOB',{required:{value: true,message: "Date of birth is required!",}})} value={DOB} id="DOB" onChange={handleInputChnage} placeholder='Date of Birth' />
+                  {errors.DOB && errors.DOB.message ? (
+                          <Box textAlign={'left'} fontSize={'12px'} py={1} maxH={'0px'} color={'red'}>
+                            {errors.DOB.message}
+                          </Box>
+                        ) : (null)
+                      }
                 </GridItem>
 
                 <GridItem>
                   <FormLabel>Academic Year</FormLabel>
-                  <Input type="text" name="academicYear" {...register('academicYear')} value={academicYear} id="academicYear" onChange={handleInputChnage} placeholder='Academic Year' />
-                  <span>{errors.academicYear && <p style={{ color: 'red' }}>Academic Year is required!</p>}</span>
+                  <Input type="text" name="academicYear" {...register('academicYear',{required:{value: true,message: "Academic Year is required!",}})} value={academicYear} id="academicYear" onChange={handleInputChnage} placeholder='Academic Year' />
+                  {errors.academicYear && errors.academicYear.message ? (
+                          <Box textAlign={'left'} fontSize={'12px'} py={1} maxH={'0px'} color={'red'}>
+                            {errors.academicYear.message}
+                          </Box>
+                        ) : (null)
+                      }
                 </GridItem>
 
               </SimpleGrid>
               <FormLabel marginTop={5}>Address</FormLabel>
-              <Textarea type="text" name="address" {...register('address')} value={address} id="address" onChange={handleInputChnage} padding={5} placeholder={'Address'} rows={3} />
-              <span>{errors.address && <p style={{ color: 'red' }}>Address is required!</p>}</span>
+              <Textarea type="text" name="address" {...register('address',{required:{value: true,message: "Address is required!",}})} value={address} id="address" onChange={handleInputChnage} padding={5} placeholder={'Address'} rows={3} />
+              {errors.address && errors.address.message ? (
+                          <Box textAlign={'left'} fontSize={'12px'} py={1} maxH={'0px'} color={'red'}>
+                            {errors.address.message}
+                          </Box>
+                        ) : (null)
+                      }
               <Box marginTop={5}><span><input type='checkbox' /> I declare that all the above mentioned information is correct</span></Box>
               <Button marginTop={5} type='submit'>Register</Button>
             </FormControl>
