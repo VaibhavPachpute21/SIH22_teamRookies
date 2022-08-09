adcb4bb9d4abbc9ef0da72f57946938313fa52a1
unknown <pandeysandeep1190@gmail.com>
2022-08-09 23:14:57 +0530

    vstack

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import './Home.css';
 import { GrLocation } from 'react-icons/gr'
 import { FiPhone } from 'react-icons/fi';
 import { AiOutlineMail } from 'react-icons/ai';
-import { Divider, Flex, Box, Image, Button, Center, GridItem, SimpleGrid, Heading, Grid, Text, Container, Icon } from '@chakra-ui/react'
+import { Divider, Flex, Box, Image, Button, Center, GridItem, SimpleGrid, Heading, Grid, Text, Container, Icon, VStack, FormControl, Input, HStack } from '@chakra-ui/react'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
 
 export default function Home() {
 
+    const [SlideIndex, setSlideIndex] = useState(0)
 
+    const HandleClick = (index) => {
+        console.log(index)
+    }
 
     return (
 
-        <Box w="100%" h="90vh">
+        <Box w="100%" overflowX={'hidden'} minH="140vh">
             <Carousel
+                onChange={(i) => { setSlideIndex(i) }}
                 autoPlay={true}
                 infiniteLoop={true}
                 showArrows={true}
@@ -27,9 +32,55 @@ export default function Home() {
                     <img
                         style={{ "background-size": 'cover', "background-position": "center", "height": "100%", "width": "100%" }}
                         src="./image/s1.jpg" />
-                        <Box bg="red.200">
-                            Hello
-                        </Box>
+                    {
+                        SlideIndex == 0 ? (
+                            <Flex
+                                borderTop={'6px solid #5A4FCF'}
+
+                                w={'30%'} height={'40vh'} bg={'white'}
+                                borderRadius={'xl'}
+                                alignSelf={"end"} position={'absolute'} marginTop={'-55vh'} right={10}>
+                                <VStack w="100%" h="100%" >
+
+
+                                    <form style={{ width: "90%", height: "80%", display: 'flex', alignItems: 'center' }}>
+                                        <FormControl w="100%" h="100%" py={10}>
+                                            <SimpleGrid spacing={10} columns={1}>
+                                                <GridItem colSpan={1}>
+
+                                                </GridItem>
+
+                                                <GridItem colSpan={1}>
+                                                    <Input
+                                                        size="lg"
+                                                        type="email"
+                                                        placeholder="Enter email"
+                                                    />
+                                                </GridItem>
+
+                                                <GridItem colSpan={1}>
+                                                    <Input
+                                                        size="lg"
+                                                        type="email"
+                                                        placeholder="Enter password"
+                                                    />
+                                                </GridItem>
+
+                                                <GridItem colSpan={1}>
+                                                    <Button
+                                                        bg="#5A4FCF"
+                                                        w="100%"
+                                                        color={'white'}
+                                                    >Login</Button>
+                                                </GridItem>
+                                            </SimpleGrid>
+                                        </FormControl>
+                                    </form>
+                                </VStack>
+                            </Flex>
+                        ) : (null)
+                    }
+
                 </div>
                 <div style={{ height: "70vh" }}>
                     <img
@@ -42,6 +93,58 @@ export default function Home() {
                         src="./image/s3.jpg" />
                 </div>
             </Carousel>
+            <HStack w="100%" h="90vh">
+                <Box
+                    w="70%" h="100%">
+                    <VStack spacing={5} py={9} w="100%" h="100%">
+                        <Box
+                            borderTop={'4px solid #5A4FCF'}
+                            borderRadius={'2xl'}
+                            boxShadow={'xl'}
+                            bg="white"
+                            w="90%" 
+                            h="50%">
+                            Hey
+                        </Box>
+
+                        <Box
+                            borderTop={'4px solid #5A4FCF'}
+                            borderRadius={'2xl'}
+                            boxShadow={'xl'}
+                            bg="white"
+                            w="90%" 
+                            h="50%">
+                            Hey
+                        </Box>
+
+                        <Box
+                            borderTop={'4px solid #5A4FCF'}
+                            borderRadius={'2xl'}
+                            boxShadow={'xl'}
+                            bg="white"
+                            w="90%" 
+                            h="50%">
+                            Hey
+                        </Box>
+
+                        <Box
+                            borderTop={'4px solid #5A4FCF'}
+                            borderRadius={'2xl'}
+                            boxShadow={'xl'}
+                            bg="white"
+                            w="90%" 
+                            h="50%">
+                            Hey
+                        </Box>
+                    </VStack>
+                </Box>
+
+                <Box w="30%" h="100%">
+                    Hello
+                </Box>
+            </HStack>
+
+
         </Box>
 
     );
