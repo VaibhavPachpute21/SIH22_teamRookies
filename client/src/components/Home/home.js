28ba0cef3ccc6caf9729aaf671ae0c762ee93e8c
unknown <pandeysandeep1190@gmail.com>
2022-08-09 17:43:19 +0530

    Background-position

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -4,41 +4,45 @@ import { GrLocation } from 'react-icons/gr'
 import { FiPhone } from 'react-icons/fi';
 import { AiOutlineMail } from 'react-icons/ai';
 import { Divider, Flex, Box, Image, Button, Center, GridItem, SimpleGrid, Heading, Grid, Text, Container, Icon } from '@chakra-ui/react'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
 
 export default function Home() {
 
-  
 
-  return (
-    
+
+    return (
+
         <Box w="100%" h="90vh">
-          <Carousel
-          autoPlay={true}
-          infiniteLoop={true}
-          showArrows={true}
-          showStatus={false}
-          showIndicators={false}
-          showThumbs={false}
-          >
-                <div style={{height:"70vh"}}>
+            <Carousel
+                autoPlay={true}
+                infiniteLoop={true}
+                showArrows={true}
+                showStatus={false}
+                showIndicators={false}
+                showThumbs={false}
+            >
+                <div style={{ height: "70vh" }}>
                     <img
-                    style={{"background-size":'cover',"background-position":"center","height":"100%","width":"100%"}}
-                    src="./image/backimg1.jpg" />
-                    <p className="legend">Legend 1</p>
+                        style={{ "background-size": 'cover', "background-position": "center", "height": "100%", "width": "100%" }}
+                        src="./image/s1.jpg" />
+                        <Box bg="red.200">
+                            Hello
+                        </Box>
                 </div>
-                <div style={{height:"50vh"}}>
-                    <img src="./image/bg1.jpg" />
-                    <p className="legend">Legend 2</p>
+                <div style={{ height: "70vh" }}>
+                    <img
+                        style={{ "background-size": 'cover', "background-position": "center", "height": "100%", "width": "100%" }}
+                        src="./image/s2.jpg" />
                 </div>
-                <div style={{height:"50vh"}}>
-                    <img src="./image/hacker.jpg" />
-                    <p className="legend">Legend 3</p>
+                <div style={{ height: "70vh" }}>
+                    <img
+                        style={{ "background-size": 'cover', "background-position": "center", "height": "100%", "width": "100%" }}
+                        src="./image/s3.jpg" />
                 </div>
             </Carousel>
         </Box>
-   
-  );
+
+    );
 }
